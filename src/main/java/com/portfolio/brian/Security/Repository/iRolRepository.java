d9cc14d9ea423207bbfa6f80b5d73192f43541ef
BrianGalarza <briangalarza71@gmail.com>
2022-10-21 01:24:25 -0300

    añadido JWT

---
+++ src/main/java/com/portfolio/brian/Security/Repository/iRolRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.brian.Security.Repository;
+
+import com.portfolio.brian.Security.Entity.Rol;
+import com.portfolio.brian.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer>{
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
