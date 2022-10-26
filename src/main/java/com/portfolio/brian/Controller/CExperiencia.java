a1276dd16d83de81c8cec71fda27e93589006f76
BrianGalarza <briangalarza71@gmail.com>
2022-10-26 18:06:42 -0300

    corregido errores

--- src/main/java/com/portfolio/brian/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/brian/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("explab")
+@RequestMapping("/explab")
 @CrossOrigin(origins = "https://frontendbrian.web.app")
 public class CExperiencia {
 
